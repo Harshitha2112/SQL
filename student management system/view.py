ee3afbb9197f0a9d968ea1eb607ed6444a044ce3
HARSHITHA U <harshithaa2112@gmail.com>
2026-08-08 21:51:49 +0530

    view file is uploaded

---
+++ student management system/view.py
@@ -0,0 +1,142 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import ttk,messagebox
+import sqlite3
+
+class viewclass:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Student result Management System")
+        self.root.geometry("1350x600+50+170")    # height and width
+        self.root.config(bg="white")
+        self.root.focus_force()
+
+        #------title-------
+        title=Label(self.root,text="View Result Details",font=("goudy old style",20,"bold"),bg="orange",fg="white").place(x=10,y=15,width=1330,height=35)
+
+
+
+#------------------variables----------------------------
+        self.var_search=StringVar()
+        self.var_id=""
+
+#--------------------------search--------------------------
+
+        lbl_search=Label(self.root,text="Search by Roll. No",font=("goudy old style",20,"bold"),bg="white").place(x=300,y=100)
+        txt_search=Entry(self.root,textvariable=self.var_search,font=("goudy old style",20,"bold"),bg="lightblue").place(x=550,y=100,width=150)
+
+        btn_search = Button(self.root, text="Search", font=("gaudy old style", 20, "bold"), bg="#03a9f4", fg="white", cursor="hand2",command=self.search)
+        btn_search.place(x=730, y=100, width=120, height=35)
+
+        btn_clear = Button(self.root, text="Clear", font=("gaudy old style", 20, "bold"), bg="grey", fg="white", cursor="hand2",command=self.clear)
+        btn_clear.place(x=880, y=100, width=100, height=35)
+
+
+
+
+#----------------------- result labels------------------------------
+
+        lbl_roll=Label(self.root,text="Roll No.",font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE).place(x=150,y=230,width=150,height=50)
+        lbl_name=Label(self.root,text="Name",font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE).place(x=300,y=230,width=150,height=50)
+        lbl_course=Label(self.root,text="Course",font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE).place(x=450,y=230,width=150,height=50)
+        lbl_marks_ob=Label(self.root,text="Marks Obtained",font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE).place(x=600,y=230,width=150,height=50)
+        lbl_full_marks=Label(self.root,text="Full Marks",font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE).place(x=750,y=230,width=150,height=50)
+        lbl_per=Label(self.root,text="Percentage",font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE).place(x=900,y=230,width=150,height=50)
+
+#----------------------------empty box----------------------------
+
+
+        self.roll=Label(self.root,font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE)
+        self.roll.place(x=150,y=280,width=150,height=50)
+
+        self.name=Label(self.root,font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE)
+        self.name.place(x=300,y=280,width=150,height=50)
+
+        self.course=Label(self.root,font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE)
+        self.course.place(x=450,y=280,width=150,height=50)
+
+        self.marks_ob=Label(self.root,font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE)
+        self.marks_ob.place(x=600,y=280,width=150,height=50)
+
+        self.full_marks=Label(self.root,font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE)
+        self.full_marks.place(x=750,y=280,width=150,height=50)
+
+        self.per=Label(self.root,font=("goudy old style",15,"bold"),bd=2,bg="white",relief=GROOVE)
+        self.per.place(x=900,y=280,width=150,height=50)
+
+
+#-------------------------delete button---------------
+
+
+        btn_delete = Button(self.root, text="Delete", font=("gaudy old style", 20, "bold"), bg="red", fg="white", cursor="hand2",command=self.delete)
+        btn_delete.place(x=550, y=350, width=120, height=35)
+
+
+#------------------------------------------------------------
+
+
+    def search(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor()
+        try:
+            if self.var_search.get()=="":
+                messagebox.showerror("Error","Roll No. should be required",parent=self.root)
+            else:
+                cur.execute("select * from result where roll=?", (self.var_search.get(),))
+                row = cur.fetchone()
+                if row != None:
+                    self.var_id=row[0]
+                    self.roll.config(text=row[1])
+                    self.name.config(text=row[2])
+                    self.course.config(text=row[3])
+                    self.marks_ob.config(text=row[4])
+                    self.full_marks.config(text=row[5])
+                    self.per.config(text=row[6])
+
+                else:
+                    messagebox.showerror("Error", "no record found", parent=self.root)
+
+        except Exception as ex:
+            messagebox.showerror("Error", "NO record found", parent=self.root)
+
+    def clear(self):
+        self.var_id=""
+        self.roll.config(text="")
+        self.name.config(text="")
+        self.course.config(text="")
+        self.marks_ob.config(text="")
+        self.full_marks.config(text="")
+        self.per.config(text="")
+        self.var_search.set("")
+
+
+    def delete(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor()
+        try:
+            if self.var_id== "":
+                messagebox.showerror("Error", " search student id first",
+                                     parent=self.root)  # it shows error when course name is empty
+            else:
+
+                cur.execute("select * from result where rid=?", (self.var_id,))
+                row = cur.fetchone()
+
+                if row == None:
+                    messagebox.showerror("Error", "invalid student result", parent=self.root)
+                else:
+                    op = messagebox.askyesno("confirm", "do you really want to delete?", parent=self.root)
+                    if op == True:
+                        cur.execute("delete from result where rid=?", (self.var_id,))
+                        con.commit()
+                        messagebox.showinfo("delete", "Result deleted successfully", parent=self.root)
+                        self.clear()
+
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to {str(ex)}")
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=viewclass(root)
+    root.mainloop()
